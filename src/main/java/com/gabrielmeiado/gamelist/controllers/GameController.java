41d4b18a7fc81b0236b0b140b2484df5962ee2f8
Meiado <gabrielmeiado@hotmail.com>
2023-07-12 14:22:07 -0300

    get game by id

--- src/main/java/com/gabrielmeiado/gamelist/controllers/GameController.java
+++ src/main/java/com/gabrielmeiado/gamelist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gabrielmeiado.gamelist.dto.GameDTO;
 import com.gabrielmeiado.gamelist.dto.GameMinDTO;
 import com.gabrielmeiado.gamelist.services.GameService;
 
@@ -21,5 +23,11 @@ public class GameController {
   public List<GameMinDTO> findAll(){
     List<GameMinDTO> list = gameService.findAll();
     return list;
+  }
+
+  @GetMapping(value = "/{id}")
+  public GameDTO findOne(@PathVariable Long id){
+    GameDTO game = gameService.findOne(id);
+    return game;
   }
 }
--- src/main/java/com/gabrielmeiado/gamelist/services/GameService.java
+++ src/main/java/com/gabrielmeiado/gamelist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.gabrielmeiado.gamelist.dto.GameDTO;
 import com.gabrielmeiado.gamelist.dto.GameMinDTO;
 import com.gabrielmeiado.gamelist.entities.Game;
 import com.gabrielmeiado.gamelist.repositories.GameRepository;
@@ -15,8 +17,15 @@ public class GameService {
   @Autowired
   private GameRepository gameRepository;
 
+  @Transactional(readOnly = true)
   public List<GameMinDTO> findAll() {
     List<Game> list = gameRepository.findAll();
     return list.stream().map(o -> new GameMinDTO(o)).toList();
+  }
+
+  @Transactional(readOnly = true)
+  public GameDTO findOne(Long id){
+    Game game = gameRepository.findById(id).get();
+    return new GameDTO(game);
   }
 }
