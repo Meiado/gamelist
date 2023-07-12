4a2bd02d1288a1f0e64e3c6cea0c4448be5c43f9
Meiado <gabrielmeiado@hotmail.com>
2023-07-12 11:24:03 -0300

    first commit

---
+++ src/test/java/com/gabrielmeiado/gamelist/GamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielmeiado.gamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
