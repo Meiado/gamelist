41c26081d5bd43ad4803134d6e6228fa54d024ec
Meiado <gabrielmeiado@hotmail.com>
2023-07-12 13:58:40 -0300

    domain model

--- src/main/java/com/gabrielmeiado/gamelist/repositories/GameRepository.java
+++ src/main/java/com/gabrielmeiado/gamelist/repositories/GameRepository.java
@@ -1,7 +1,6 @@
 package com.gabrielmeiado.gamelist.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.gabrielmeiado.gamelist.entities.Game;
 
 public interface GameRepository extends JpaRepository<Game, Long>{
