f11980cc2d207a7307b705526945243814ffabc1
Meiado <gabrielmeiado@hotmail.com>
2023-07-14 00:50:49 -0300

    Change games position on list

--- src/main/java/com/gabrielmeiado/gamelist/services/GameListService.java
+++ src/main/java/com/gabrielmeiado/gamelist/services/GameListService.java
@@ -8,13 +8,18 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.gabrielmeiado.gamelist.dto.GameListDTO;
 import com.gabrielmeiado.gamelist.entities.GameList;
+import com.gabrielmeiado.gamelist.projections.GameMinProjection;
 import com.gabrielmeiado.gamelist.repositories.GameListRepository;
+import com.gabrielmeiado.gamelist.repositories.GameRepository;
 
 @Service
 public class GameListService {
 
   @Autowired
   private GameListRepository gameListRepository;
+
+  @Autowired
+  private GameRepository gameRepository;
 
   @Transactional(readOnly = true)
   public List<GameListDTO> findAll() {
@@ -26,5 +31,18 @@ public class GameListService {
   public GameListDTO findOne(Long id){
     GameList game = gameListRepository.findById(id).get();
     return new GameListDTO(game);
+  }
+
+  @Transactional
+  public void move(Long listId, int sourceIndex, int destinationIndex) {
+    List<GameMinProjection> list = gameRepository.searchByList(listId);
+    GameMinProjection obj = list.remove(sourceIndex);
+    list.add(destinationIndex, obj);
+
+    int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+    int max = sourceIndex > destinationIndex ? sourceIndex : destinationIndex;
+    for (int i = min; i <= max; i++) {
+      gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+    }
   }
 }
